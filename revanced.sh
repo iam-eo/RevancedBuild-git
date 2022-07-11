41dde3304f028b9857eca2b410b9d7efb7488882
iam-eo <ian.eric.ong@gmail.com>
2022-07-12 02:28:39 +0800

    Update revanced.sh

--- revanced.sh
+++ revanced.sh
@@ -1,1 +1,13 @@
-java -jar revanced-cli-2.3.2-all.jar -a YouTube17.25.35.apk -b revanced-patches-2.10.1.jar -m app-release-unsigned-0.21.8.apk -o new-build.apk -e background-play -e exclusive-audio-playback -e codecs-unlock -e upgrade-button-remover -e tasteBuilder-remover -c
+#!/bin/sh
+
+
+#cli="revanced-cli-2.5.3-all.jar"
+#patches="revanced-patches-2.13.2.jar"
+#integration="app-release-unsigned-0.22.1.apk"
+#yt="Youtube_17.26.35.apk"
+
+#java -jar cli -a yt -b patches -m integration -o new-build.apk -c -e background-play -e exclusive-audio-playback -e codecs-unlock -e upgrade-button-remover -e tasteBuilder-remover
+
+java -jar revanced-cli-2.5.3-all.jar -a Youtube_17.26.35.apk -b revanced-patches-2.13.2.jar -m app-release-unsigned-0.22.1.apk -o new-build.apk -c -e background-play -e exclusive-audio-playback -e codecs-unlock -e upgrade-button-remover -e tasteBuilder-remover
+
+
